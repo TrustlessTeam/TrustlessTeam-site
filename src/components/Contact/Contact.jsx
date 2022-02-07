98b358e44048c68bfed8d97bfc6aa5145f163d7e
Edd Norris <eddnorris@2bitdev.com>
2022-02-07 06:29:28 -0800

    replaced contact us with the staking information

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -11,19 +11,22 @@ const Contact = () => {
   return (
     <section id="contact">
       <Container>
-        <Title title="Contact" />
+        <Title title="STAKE YOUR $ONE WITH US" />
         <Fade bottom duration={1000} delay={800} distance="30px">
           <div className="contact-wrapper">
+          <p className="contact-wrapper__text">
+              {cta || 'TrustlessTeam has recently launched a Staking Validator on Harmony! '}
+            </p>
             <p className="contact-wrapper__text">
-              {cta || 'Would you like to work with us? Awesome!'}
+              {cta || 'Your support would be very much appreciated! '}
             </p>
             <a
               target="_blank"
               rel="noopener noreferrer"
               className="cta-btn cta-btn--resume"
-              href={email ? `mailto:${email}` : 'https://github.com/TrustlessTeam/fishfight'}
+              href={'https://staking.harmony.one/validators/mainnet/one1w9g7j40ctzlffmjrp6nmypdupsmyh0u90yn9xs'}
             >
-              {btn || "Let's Talk"}
+              {btn || "STAKE NOW!"}
             </a>
           </div>
         </Fade>
